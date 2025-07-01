d1de1502c863d0d2c3976c7cc9b6fe161c968121
EroInnovations <eroinnovations9@gmail.com>
2025-07-01 22:37:55 +0300

    Updates

--- Project/Project.js
+++ Project/Project.js
@@ -1,13 +1,5 @@
 const NOVASTART=()=>{
 
-    const H="f94ff247-90d3-498a-b337-41c98b66c6eb";
-
-    const D=["ee","Haik"]
-
     CLEAR("");
-
-    UPDATEDATA("https://docs.google.com/spreadsheets/d/1FaYjZ-wrvulW0AZQ6VQvEKYCp-gYqykahzGf42zi1v0/edit?usp=sharing","tetes",H,D,(data)=>{
-        console.log(data)
-    })
 
 };
