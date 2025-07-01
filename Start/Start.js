7ac5365a9bca1653452464fb13197f74b94e9923
EroInnovations <eroinnovations9@gmail.com>
2025-07-01 22:57:39 +0300

    Updates

--- Start/Start.js
+++ Start/Start.js
@@ -1,12 +1,15 @@
 import { CLOUDCONNECTION } from "../Connection/CloudConnection.js";
 import { FUNCTIONS } from "../Connection/Functions.js";
 import { COMPONENTSCONNECTION } from "../Library/Components/Connection/Connection.js";
+import { PLUGINSCONECTION } from "../Library/Plugins/Connection/Connection.js";
 
 export const START=()=>{
 
     FUNCTIONS();
 
     COMPONENTSCONNECTION();
+
+    PLUGINSCONECTION();
 
     CLOUDCONNECTION('../Project/Project.js');
    
