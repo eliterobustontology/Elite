a04f37e963c3af4b8b65b334663e7c52b0030c4c
EroInnovations <eroinnovations9@gmail.com>
2025-07-01 22:07:50 +0300

    Updates

--- Project/Project.js
+++ Project/Project.js
@@ -1,8 +1,14 @@
+import { INSERTDATA } from "../Library/Functions/DataBase/InsertData/InsertData";
+
 const NOVASTART=()=>{
+
+    const H=["name","age"];
+
+    const D=["ee","rr"]
 
     CLEAR("");
 
-    GETDATA("https://docs.google.com/spreadsheets/d/1FaYjZ-wrvulW0AZQ6VQvEKYCp-gYqykahzGf42zi1v0/edit?usp=sharing","tetes",(data)=>{
+    INSERTDATA("https://docs.google.com/spreadsheets/d/1FaYjZ-wrvulW0AZQ6VQvEKYCp-gYqykahzGf42zi1v0/edit?usp=sharing","tetes",H,D,(data)=>{
         console.log(data)
     })
 
