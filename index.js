be5d3bb6d62aea89579dc256ec2fb3fec2ab991b
EroInnovations <eroinnovations9@gmail.com>
2025-07-01 22:51:53 +0300

    Updates

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-localStorage.setItem('NAME', '8e66d79d-84b7-4ba0-9d60-0e74189675c45');
-localStorage.setItem('Environment', 'Development');
-const ROUTEJS=(DATA)=>{ const styleElement = document.createElement("script"); styleElement.textContent = DATA; document.head.appendChild(styleElement);};
-ROUTEJS(localStorage.getItem('NOVA'));
-const CloudShipping = () => {
-    import('./Start/Start.js')
-    .then(module => {
-        if (typeof module.START === 'function') {
-        module.START();
-        } else {
-            console.error('START is not defined in the module');
-        }
-    })
-    .catch(error => {
-        console.error('Error loading the module:', error);
-    });
-};
-if (localStorage.getItem('Updates')) {
-    NOVASTART();
-    } else {
-    CloudShipping();
-};
-
 const style = document.createElement('style');
 style.textContent = `
 html,body {
@@ -64,3 +41,33 @@ html,body {
 }
 `;
 document.head.appendChild(style);
+document.querySelector("body").innerHTML=`
+ <div class="loading-container">
+    <div>
+        <div class="spinner"></div>
+        <p class="loading-text">Please Wait</p>
+    </div>
+  </div>
+`;
+localStorage.setItem('NAME', '8e66d79d-84b7-4ba0-9d60-0e74189675c45');
+localStorage.setItem('Environment', 'Development');
+const ROUTEJS=(DATA)=>{ const styleElement = document.createElement("script"); styleElement.textContent = DATA; document.head.appendChild(styleElement);};
+ROUTEJS(localStorage.getItem('NOVA'));
+const CloudShipping = () => {
+    import('./Start/Start.js')
+    .then(module => {
+        if (typeof module.START === 'function') {
+        module.START();
+        } else {
+            console.error('START is not defined in the module');
+        }
+    })
+    .catch(error => {
+        console.error('Error loading the module:', error);
+    });
+};
+if (localStorage.getItem('Updates')) {
+    NOVASTART();
+    } else {
+    CloudShipping();
+};
