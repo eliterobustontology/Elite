1d6f09f37fc6deec33abd676408168b6a44f8c10
EroInnovations <eroinnovations9@gmail.com>
2025-07-01 22:32:52 +0300

    Updates

--- Library/Functions/DataBase/AutoRun/AutoRun.js
+++ Library/Functions/DataBase/AutoRun/AutoRun.js
@@ -1,13 +1,33 @@
 export const AUTORUN=()=>{
-    import('./Start/Start.js')
-    .then(module => {
-        if (typeof module.START === 'function') {
-        module.START();
-        } else {
-            console.error('START is not defined in the module');
-        }
-    })
-    .catch(error => {
-        console.error('Error loading the module:', error);
-    });
+
+    if (localStorage.getItem("Environment") === "Development" ) {
+        
+        import('./Start/Start.js')
+        .then(module => {
+            if (typeof module.START === 'function') {
+            module.START();
+            } else {
+                console.error('START is not defined in the module');
+            }
+        })
+        .catch(error => {
+            console.error('Error loading the module:', error);
+        });
+
+    } else {
+
+        import('./Start/Start.js')
+        .then(module => {
+            if (typeof module.START === 'function') {
+            module.START();
+            } else {
+                console.error('START is not defined in the module');
+            }
+        })
+        .catch(error => {
+            console.error('Error loading the module:', error);
+        });
+        
+    }
+
 }
