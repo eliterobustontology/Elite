235299f70689a6d7ab7b59ccb07762f25c46c31b
EroInnovations <eroinnovations9@gmail.com>
2025-07-02 21:03:12 +0300

    Updates

---
+++ Environment/AndroidEnv.js
@@ -0,0 +1,24 @@
+import { CLOUDSTART } from "../Connection/Cloud.js";
+import { NOVA } from "../Connection/CloudStart.js";
+
+export const WEBENV=()=>{
+
+    fetch(PATH)
+    .then(res =>res.text())
+    .then(data =>{
+
+        localStorage.setItem('PROJECT',data);
+
+        NOVA();
+
+        CLOUDSTART();
+
+    })
+    .catch(error=>{console.log(error)}
+    );
+
+    console.log(element);
+
+    return;
+
+};
