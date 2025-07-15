1ba7f95f951b774f979ce994586d5df6a310283f
EroInnovations <eroinnovations9@gmail.com>
2025-07-15 20:21:29 +0300

    Updates

--- Library/Assets/Connection/Connection.js
+++ Library/Assets/Connection/Connection.js
@@ -1,13 +1,15 @@
 import { WHITEMENUICON } from "../DataBase/WhiteIcons/Menu/Menu.js";
+import { WHITEPHONEICON } from "../DataBase/WhiteIcons/Phone/Phone.js";
 
 export const ASSETSCONNECTION=()=>{
 const DATA=`const WHITEMENUICON="${WHITEMENUICON}";
+const WHITEPHONEICON="${WHITEPHONEICON}";
 `;
     
 localStorage.setItem('ASSETS',DATA)
 
 };
 `
-const WHITEMENUICON="${WHITEMENUICON}";
+const WHITEPHONEICON="${WHITEPHONEICON}";
 
 `
--- Project/Project.js
+++ Project/Project.js
@@ -1,12 +1,19 @@
 const NOVASTART=()=>{
 
-  
+  APPMODE("White")
 
   CLEAR();
 
-  HEADER("","forestgreen",()=>{
+  HEADER("","forestgreen",(ELEMENT)=>{
+
+    TEXT(ELEMENT,"h1","white","auto 1%","20px","Doctor Mjombali",()=>{
+
+    });
+
+    IMAGE(ELEMENT,WHITEPHONEICON,"transparent","25px","25px","auto auto auto 45%",()=>{
+
+    });
 
   });
-
 
 };
