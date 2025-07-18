bbc89fc095d788e958cbc0d7ec417150d8052991
EroInnovations <eroinnovations9@gmail.com>
2025-07-18 15:57:41 +0300

    Updates

--- Library/Components/DataBase/Image/Image.js
+++ Library/Components/DataBase/Image/Image.js
@@ -9,6 +9,7 @@ export const IMAGE=(HOLDER,SRC,COLOR,HEIGHT,WIDTH,MARGIN,callback)=>{
         STYLED(ELEMENTS,"height",HEIGHT||"100%");
         STYLED(ELEMENTS,"margin",MARGIN||"auto");
         STYLED(ELEMENTS,"background",COLOR);
+        STYLED(ELEMENTS,"object=dit",'cover');
        
         ELEMENTS.src=SRC||"#";
         
