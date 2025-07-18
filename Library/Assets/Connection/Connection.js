086940c93d1155bdbb61654ba23106c40b985d29
EroInnovations <eroinnovations9@gmail.com>
2025-07-18 16:01:19 +0300

    Updates

--- Library/Assets/Connection/Connection.js
+++ Library/Assets/Connection/Connection.js
@@ -1,4 +1,5 @@
 import { WHITEBACKICON } from "../DataBase/WhiteIcons/Back/Back.js";
+import { WHITEINSTAGRAMICON } from "../DataBase/WhiteIcons/Instagram/Instagram.js";
 import { WHITELOCATIONICON } from "../DataBase/WhiteIcons/Location/Location.js";
 import { WHITEMENUICON } from "../DataBase/WhiteIcons/Menu/Menu.js";
 import { WHITEPHONEICON } from "../DataBase/WhiteIcons/Phone/Phone.js";
@@ -10,12 +11,13 @@ const WHITEPHONEICON="${WHITEPHONEICON}";
 const WHITEWHATSAPPICON="${WHITEWHATSAPPICON}";
 const WHITEBACKICON="${WHITEBACKICON}";
 const WHITELOCATIONICON="${WHITELOCATIONICON}";
+const WHITEINSTAGRAMICON="${WHITEINSTAGRAMICON}";
 `;
     
 localStorage.setItem('ASSETS',DATA)
 
 };
 `
-const WHITELOCATIONICON="${WHITELOCATIONICON}";
+const WHITEINSTAGRAMICON="${WHITEINSTAGRAMICON}";
 
 `
