83a161dbbc77c66d4fb4ba5337c001971900428f
EroInnovations <eroinnovations9@gmail.com>
2025-07-19 10:00:27 +0300

    Updates

--- Library/Assets/Connection/Connection.js
+++ Library/Assets/Connection/Connection.js
@@ -1,9 +1,17 @@
+import { WHITEADDICON } from "../DataBase/WhiteIcons/Add/Add.js";
+import { WHITEADDPERSONICON } from "../DataBase/WhiteIcons/AddPerson/AddPerson.js";
+import { WHITEANDROIDICON } from "../DataBase/WhiteIcons/Android/Android.js";
+import { WHITEAPPICON } from "../DataBase/WhiteIcons/App/AppIcon.js";
 import { WHITEBACKICON } from "../DataBase/WhiteIcons/Back/Back.js";
 import { WHITEFACEBOOKICON } from "../DataBase/WhiteIcons/FaceBook/FaceBook.js";
+import { WHITEINFOICON } from "../DataBase/WhiteIcons/Info/Info.js";
 import { WHITEINSTAGRAMICON } from "../DataBase/WhiteIcons/Instagram/Instagram.js";
+import { WHITELEFTBACKICON } from "../DataBase/WhiteIcons/LeftBack/LeftBack.js";
 import { WHITELOCATIONICON } from "../DataBase/WhiteIcons/Location/Location.js";
 import { WHITEMENUICON } from "../DataBase/WhiteIcons/Menu/Menu.js";
 import { WHITEPHONEICON } from "../DataBase/WhiteIcons/Phone/Phone.js";
+import { WHITESHOPPINGCART } from "../DataBase/WhiteIcons/ShoppingCart/ShoppingCart.js";
+import { WHITESONGBACKICON } from "../DataBase/WhiteIcons/SongBack/SongBack.js";
 import { WHITEWHATSAPPICON } from "../DataBase/WhiteIcons/Whatsapp/Whatsapp.js";
 
 export const ASSETSCONNECTION=()=>{
@@ -14,12 +22,20 @@ const WHITEBACKICON="${WHITEBACKICON}";
 const WHITELOCATIONICON="${WHITELOCATIONICON}";
 const WHITEINSTAGRAMICON="${WHITEINSTAGRAMICON}";
 const WHITEFACEBOOKICON="${WHITEFACEBOOKICON}";
+const WHITEADDICON="${WHITEADDICON}";
+const WHITEINFOICON="${WHITEINFOICON}";
+const WHITESHOPPINGCART="${WHITESHOPPINGCART}";
+const WHITEADDPERSONICON="${WHITEADDPERSONICON}";
+const WHITEANDROIDICON="${WHITEANDROIDICON}";
+const WHITEAPPICON="${WHITEAPPICON}";
+const WHITELEFTBACKICON="${WHITELEFTBACKICON}";
+const WHITESONGBACKICON="${WHITESONGBACKICON}";
 `;
     
 localStorage.setItem('ASSETS',DATA)
 
 };
 `
-const WHITEFACEBOOKICON="${WHITEFACEBOOKICON}";
+const WHITESONGBACKICON="${WHITESONGBACKICON}";
 
 `
