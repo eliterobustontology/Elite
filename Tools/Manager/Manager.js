7661dcc3ae2f3acded0d09600b2e2bde488dff0b
EroInnovations <eroinnovations9@gmail.com>
2025-07-30 15:08:36 +0300

    Updates

--- Tools/Manager/Manager.js
+++ Tools/Manager/Manager.js
@@ -2,4 +2,44 @@ const HOMEPAGE=()=>{
 
     APPMODE("#333333");
 
+    ROUTE("",MOBILELOGINPAGE,"MOBILELOGINPAGE");
+
+};
+
+const MOBILELOGINPAGE=()=>{
+
+    CLEAR();
+
+    TEXT("","h1","#ffffff","5% auto","25px","Elite Manager",()=>{
+
+    });
+
+    TEXT("","h1","#ffffff","4% auto","16px","Admin Control",()=>{
+
+    });
+
+    INPUT("","95%","32px","","#ffffff","email","Enter Admin Email",(ELEMENT)=>{
+
+        STYLED(ELEMENT,"background","transparent");
+        STYLED(ELEMENT,"border","1px solid forestgreen");
+
+    });
+
+    INPUT("","95%","32px","3% auto","#ffffff","password","Enter Admin Password",(ELEMENT)=>{
+
+        STYLED(ELEMENT,"background","transparent");
+        STYLED(ELEMENT,"border","1px solid forestgreen");
+
+    });
+
+    DIV("","98%","50px","forestgreen","inline-flex","hidden","2% auto",(ELEMENTS)=>{
+
+        STYLED(ELEMENTS,"border-radius","5px");
+
+        TEXT(ELEMENTS,"h1","#ffffff","5% auto","20px","Sign In",()=>{
+
+        });
+
+    });
+
 };
