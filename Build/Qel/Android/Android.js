c30db36b032a1f050f0349e035e2edf69af20a8e
EroInnovations <eroinnovations9@gmail.com>
2025-08-01 09:49:34 +0300

    Updates

--- Build/Qel/Android/Android.js
+++ Build/Qel/Android/Android.js
@@ -20,11 +20,39 @@ const HOMEPAGE=()=>{
 
     CLEAR("");
 
-    DIV("","","auto","orange","block","auto","",(ELEMENT)=>{
+    DIV("","","auto","transparent","block","auto","",(ELEMENT)=>{
 
         STYLED(ELEMENT,"position","absolute");
         STYLED(ELEMENT,"bottom","50px");
         STYLED(ELEMENT,"top","0px");
+
+        DIV(ELEMENT,"100%","50px","transparent","block","hidden","",(ELEMENTS)=>{
+
+            STYLED(ELEMENTS,"border-bottom","1px solid green");
+
+            DIV(ELEMENTS,"20%","50px","transparent","inline-flex","hidden","",(ELEMENTSE)=>{
+
+                STYLED(ELEMENTSE,"position","absolute");
+                STYLED(ELEMENTSE,"left","0px");
+
+                ICON(ELEMENTSE,QEL,"transparent","","","",(ELEMENTIS)=>{
+
+                });
+
+            });
+
+            DIV(ELEMENTS,"80%","50px","transparent","inline-flex","hidden","",(ELEMENTSE)=>{
+
+                STYLED(ELEMENTSE,"position","absolute");
+                STYLED(ELEMENTSE,"right","0px");
+
+                ROUNDINPUT(ELEMENTSE,"95%","35px","5px","auto","green","text","Search Products",()=>{
+
+                });
+
+            });
+
+        });
 
     });
 
