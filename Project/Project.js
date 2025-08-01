2aebef20519ba058733b8f91ff7bc3a05d12a127
EroInnovations <eroinnovations9@gmail.com>
2025-08-01 09:09:07 +0300

    Updates

--- Project/Project.js
+++ Project/Project.js
@@ -1,6 +1,33 @@
 const NOVASTART=()=>{
 
-    CLEAR();
+    CLEAR("");
 
+    ROUTE("",HOMEPAGE,"HOMEPAGE");
+
+};
+
+const HOMEPAGE=()=>{
+
+    CLEAR("");
+
+    FOOTER("","forestgreen",(ELEMENT)=>{
+
+        ICON(ELEMENT,WHITEGRIDICON,"transparent","","","",()=>{
+
+        });
+
+        ICON(ELEMENT,WHITESAVEDICON,"transparent","","","",()=>{
+
+        });
+
+        ICON(ELEMENT,WHITESETTINGSICON,"transparent","","","",()=>{
+
+        });
+
+        ICON(ELEMENT,WHITEUSERPROFILEICON,"transparent","","","",()=>{
+
+        });
+        
+    });
 
 };
