556f24e197e7380552b4397d39f8ca65eeea7688
EroInnovations <eroinnovations9@gmail.com>
2025-08-01 09:26:50 +0300

    Updates

--- Build/Qel/Android/Android.js
+++ Build/Qel/Android/Android.js
@@ -1,6 +1,12 @@
+const LINK="https://docs.google.com/spreadsheets/d/18BUoCg4yVUrxWv8RG885ZIn2fjkURGgjIrCi6otCxFk/edit?usp=sharing";
+
 const NOVASTART=()=>{
 
-    CLEAR("");
+    DOWNLOADSAVEINDEX(LINK,"Products","Products","Products",()=>{
+
+        ROUTE("",HOMEPAGE,"HOMEPAGE");
+
+    });
 
     ROUTE("",HOMEPAGE,"HOMEPAGE");
 
@@ -8,7 +14,19 @@ const NOVASTART=()=>{
 
 const HOMEPAGE=()=>{
 
+    DOWNLOADSAVEINDEX(LINK,"Products","Products","Products",()=>{
+
+    });
+
     CLEAR("");
+
+    DIV("","","auto","orange","block","auto","",(ELEMENT)=>{
+
+        STYLED(ELEMENT,"position","absolute");
+        STYLED(ELEMENT,"bottom","50px");
+        STYLED(ELEMENT,"top","0px");
+
+    });
 
     FOOTER("","forestgreen",(ELEMENT)=>{
 
