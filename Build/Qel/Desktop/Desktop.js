ef6c37c6321c0308b95834dbfa5f0bfe038e7626
EroInnovations <eroinnovations9@gmail.com>
2025-08-03 13:11:52 +0300

    Updates

---
+++ Build/Qel/Desktop/Desktop.js
@@ -0,0 +1,41 @@
+const NOVASTART=()=>{
+
+    CONDITION(localStorage.getItem("UserData"),()=>{
+
+        ROUTE("",HOMEPAGE,"HOMEPAGE");
+
+    },()=>{
+
+        ROUTE("",LOGINPAGE,"HOMEPAGE");        
+
+    });
+
+};
+
+const HOMEPAGE=()=>{
+
+    CLEAR();
+
+};
+
+const LOGINPAGE=()=>{
+
+    CLEAR();
+
+    TEXT("","h1","#ffffff","5%","25px","Qel Manager",()=>{
+
+    });
+
+    ROUNDINPUT("","90%","50px","10px","","#ffffff","email","Enter Admin Email",()=>{
+
+    });
+
+    ROUNDINPUT("","90%","50px","10px","2% auto","#ffffff","password","Enter Admin Password",()=>{
+
+    });
+
+    BUTTON("","90%","50px","green","#ffffff","10px","Log In","2% auto",()=>{
+
+    });
+
+};
