a88d6df989e654b5c60155affe1c87a487a3beba
EroInnovations <eroinnovations9@gmail.com>
2025-08-03 13:51:20 +0300

    Updates

--- Build/Qel/Desktop/Desktop.js
+++ Build/Qel/Desktop/Desktop.js
@@ -54,6 +54,71 @@ const HOMEPAGE=()=>{
 
     });
 
+    DIV("","","auto","transparent","block","auto","",(ELEMENT)=>{
+
+        STYLED(ELEMENT,"position","absolute");
+        STYLED(ELEMENT,"top","100px");
+        STYLED(ELEMENT,"bottom","0");
+
+        GETINDEXEDDATA("Products", "Products", (element)=>{
+
+            DIV(ELEMENT,"250px","250px","transparent","inline-table","hidden","2%",(ELEMENTEIS)=>{
+
+                STYLED(ELEMENTEIS,"flex-shrink","0");
+                STYLED(ELEMENTEIS,"border-radius","5px");
+                STYLED(ELEMENTEIS,"border","1px solid green");
+                STYLED(ELEMENTEIS,"flexShrink","0");
+
+                IMAGE(ELEMENTEIS,element.ProductImage,"","100%","100%","",(ELEMENTER)=>{
+
+                    STYLED(ELEMENTER,"position","absolute");
+                    STYLED(ELEMENTER,"left","0");
+
+                });
+
+                FOOTER(ELEMENTEIS,"forestgreen",(ELEMENT)=>{
+
+                    STYLED(ELEMENT,"height","100px");
+                    STYLED(ELEMENT,"display","block");
+
+                    TEXT(ELEMENT,"h1","#FFFFFF","5%","14px",element.ProductName,(ETET)=>{
+
+                    });
+
+                    DIV(ELEMENT,"100%","50px","transparent","inline-table","hidden","",(ELEMENTEIS)=>{
+
+                        STYLED(ELEMENTEIS,"position","absolute");
+                        STYLED(ELEMENTEIS,"bottom","0");
+                        STYLED(ELEMENTEIS,"left","0");
+
+                        TEXT(ELEMENTEIS,"h1","orange","5%","14px","UGX "+element.ProductPrice,(TETST)=>{
+                                
+                            STYLED(TETST,"text-align","left");
+
+                        });
+
+                        DIV(ELEMENTEIS,"20%","50px","transparent","inline-flex","hidden","",(ELEMENTSE)=>{
+
+                            STYLED(ELEMENTSE,"position","absolute");
+                            STYLED(ELEMENTSE,"right","5px");
+                            STYLED(ELEMENTSE,"bottom","0px");
+
+                            ICON(ELEMENTSE,WHITEUNHEARTICON,"transparent","","","",(ELEMENTIS)=>{
+
+                            });
+
+                        });
+
+                    });
+
+                });
+            
+            });
+
+        });
+
+    });
+
 };
 
 const LOGINPAGE=()=>{
