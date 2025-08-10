1ce621245832c34211b09262814405bb39310742
EroInnovations <eroinnovations9@gmail.com>
2025-08-10 20:37:16 +0300

    Updates

--- Tools/Manager/Manager.js
+++ Tools/Manager/Manager.js
@@ -1,4 +1,10 @@
+const URLLINK="https://docs.google.com/spreadsheets/d/1Utfr1wkoZSRvM9TOKaTxOX6orYE8AuN2mu4dDtQmhFQ/edit?usp=sharing";
+
 const HOMEPAGE=()=>{
+
+    DOWNLOADSAVEINDEX(URLLINK,"APPMANAGER","APPMANAGER",()=>{
+
+    });
 
     APPMODE("#333333");
 
