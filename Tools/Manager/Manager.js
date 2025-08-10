97a73eaf380695944c4263f933f15528e3934d08
EroInnovations <eroinnovations9@gmail.com>
2025-08-10 21:55:04 +0300

    Updates

--- Tools/Manager/Manager.js
+++ Tools/Manager/Manager.js
@@ -32,44 +32,6 @@ const MOBILELOGINPAGE=()=>{
 
     CLEAR();
 
-    TEXT("","h1","#ffffff","5% auto","25px","Elite Manager",()=>{
-
-    });
-
-    TEXT("","h1","#ffffff","4% auto","16px","Admin Control",()=>{
-
-    });
-
-    INPUT("","95%","32px","","#ffffff","email","Enter Admin Email",(ELEMENT)=>{
-
-        STYLED(ELEMENT,"background","transparent");
-        STYLED(ELEMENT,"border","1px solid forestgreen");
-
-        EVENT(ELEMENT,"input",()=>{
-
-            console.log(data.value)
-
-        });
-
-    });
-
-    INPUT("","95%","32px","3% auto","#ffffff","password","Enter Admin Password",(ELEMENT)=>{
-
-        STYLED(ELEMENT,"background","transparent");
-        STYLED(ELEMENT,"border","1px solid forestgreen");
-
-    });
-
-    DIV("","98%","50px","forestgreen","inline-flex","hidden","2% auto",(ELEMENTS)=>{
-
-        STYLED(ELEMENTS,"border-radius","5px");
-
-        TEXT(ELEMENTS,"h1","#ffffff","auto","20px","Sign In",()=>{
-
-        });
-
-    });
-
 };
 
 const DESKTOPPAGE=()=>{
