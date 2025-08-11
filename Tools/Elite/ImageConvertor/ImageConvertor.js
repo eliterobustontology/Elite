3ed70cf68b79e800f54114b1fbcb8c1de25c4726
EroInnovations <eroinnovations9@gmail.com>
2025-08-11 13:53:28 +0300

    Updates

--- Tools/Elite/ImageConvertor/ImageConvertor.js
+++ Tools/Elite/ImageConvertor/ImageConvertor.js
@@ -1,9 +1,27 @@
 const HOMEPAGE=()=>{
 
-    APPMODE("Red");
+    APPMODE("#333333");
 
-    HEADER("","orange",(ELEMENTS)=>{
+    IMAGE("",WHITEHOMEICON,"forestgreen","50%","96%","2%",(ELEMENT)=>{
+
+        CLICK(ELEMENT,()=>{
+
+            IMAGEPICKER(ELEMENT,(data)=>{});
+
+        });
 
     });
 
+    BUTTON("","80%","50px","forestgreen","#FFFFFF","20px","Copy","3%",(ELEMENT)=>{
+
+        CLICK(ELEMENT,()=>{
+
+            COPY(sessionStorage.getItem("Imaged"));
+
+            DISPLAY(ELEMENT,"Copied");
+
+        });
+
+    });
+    
 };
