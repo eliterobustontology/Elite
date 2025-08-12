fc8629ccddd6dc06a0f03411d798e6f0cc75c27c
EroInnovations <eroinnovations9@gmail.com>
2025-08-12 09:13:30 +0300

    Updates

--- Library/Plugins/Connection/Connection.js
+++ Library/Plugins/Connection/Connection.js
@@ -1,7 +1,9 @@
 import { BATTERY } from "../DataBase/Battery/Battery.js";
+import { CAMERA } from "../DataBase/Camera/Camera.js";
 import { PICKFILE } from "../DataBase/PickFile/PickFile.js";
 import { TOASTED } from "../DataBase/Toast/Toast.js";
 import { VIBRATION } from "../DataBase/Vibration/Vibration.js";
+import { VIDEO } from "../DataBase/Video/Video.js";
 
 export const PLUGINSCONECTION=()=>{
 
@@ -9,11 +11,13 @@ const DATA=`const VIBRATION=${VIBRATION};
 const TOASTED=${TOASTED};
 const BATTERY=${BATTERY};
 const PICKFILE=${PICKFILE};
+const CAMERA=${CAMERA};
+const VIDEO=${VIDEO};
 `;
 localStorage.setItem('PLUGINS',DATA);
 };
 
 `
-const PICKFILE=${PICKFILE};
+const VIDEO=${VIDEO};
 
 `
---
+++ Library/Plugins/DataBase/Video/Video.js
@@ -0,0 +1,5 @@
+export const VIDEO=()=>{
+
+    Android.openVideoCamera();
+
+};
