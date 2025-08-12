4ae1c108178e9a4b1324445deb7238772508dc50
EroInnovations <eroinnovations9@gmail.com>
2025-08-12 13:28:44 +0300

    Updates

--- Library/Functions/DataBase/Finder/Finder.js
+++ Library/Functions/DataBase/Finder/Finder.js
@@ -1,4 +1,7 @@
 export const FINDER = (DATA, ELEMENT, ELEMENT1, ACTION) => {
+
     const user = DATA.find((item) => item[ELEMENT] === ELEMENT1);
+
     return ACTION(user ? user : false);
+    
 };
