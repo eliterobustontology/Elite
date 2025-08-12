44af5a8447768c32fd0d99f0345bc0b09fafc62e
EroInnovations <eroinnovations9@gmail.com>
2025-08-12 12:02:14 +0300

    Updates

--- Library/Functions/DataBase/AccouintChecker/AccountChecker.js
+++ Library/Functions/DataBase/AccouintChecker/AccountChecker.js
@@ -1,19 +1,37 @@
 export const ACCOUNTCHECKER = (HomeCallBack, VerificationCallBack, LoginCallBack) => {
+    
     CONDITION(
+
         localStorage.getItem("UserData"),
+
         () => {
+
             HomeCallBack();
+
         },
+
         () => {
+
             CONDITION(
+
                 localStorage.getItem("VeriifcationCode"),
+
                 () => {
+
                     VerificationCallBack();
+
                 },
+
                 () => {
+
                     LoginCallBack();
+
                 }
+
             );
+
         }
+        
     );
+
 };
--- Library/Functions/DataBase/AudioPicker/AudioPicker.js
+++ Library/Functions/DataBase/AudioPicker/AudioPicker.js
@@ -1,32 +1,63 @@
 export const AUDIOPICKER = (audioElement, callback) => {
+
     const input = document.createElement("input");
+
     input.type = "file";
+
     input.style.display = "none";
+
     input.accept = "audio/*";
+    
     document.body.appendChild(input);
+
     input.addEventListener("change", function () {
+
         var file = this.files[0];
+
         if (!file) return;
+
         var reader = new FileReader();
+
         reader.onload = function (event) {
+
             var base64Data = event.target.result;
+
             var compressedBase64Data = resizeBase64Data(base64Data, 49800);
+
             if (compressedBase64Data) {
+
                 audioElement.src = compressedBase64Data;
+
                 audioElement.play();
+
                 callback(compressedBase64Data);
+
             } else {
+
                 TOAST("Unable to resize the audio data within the character limit.");
+
             }
+
         };
+
         reader.readAsDataURL(file);
+
     });
+
     input.click();
+
     input.remove();
+
     function resizeBase64Data(base64Data, targetSize) {
+
         if (base64Data.length <= targetSize) {
+
             return base64Data;
+
         }
+
         return base64Data.substring(0, targetSize);
+
     }
+    
 };
