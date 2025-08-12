14fcd4cdb32ddcbe18e27ad0a497e0738a433f80
EroInnovations <eroinnovations9@gmail.com>
2025-08-12 11:59:54 +0300

    Updates

--- Library/Functions/DataBase/AutoRun/AutoRun.js
+++ Library/Functions/DataBase/AutoRun/AutoRun.js
@@ -3,29 +3,47 @@ export const AUTORUN=()=>{
     if (localStorage.getItem("Environment") === "Development" ) {
         
         import('./Start/Start.js')
+
         .then(module => {
+            
             if (typeof module.START === 'function') {
+
             module.START();
+
             } else {
+
                 console.error('START is not defined in the module');
+
             }
         })
         .catch(error => {
+
             console.error('Error loading the module:', error);
+
         });
 
     } else {
 
         import('https://eliterobustontology.github.io/Elite/Start/Start.js')
+
         .then(module => {
+
             if (typeof module.START === 'function') {
+
             module.START();
+
             } else {
+
                 console.error('START is not defined in the module');
+
             }
+
         })
+
         .catch(error => {
+
             console.error('Error loading the module:', error);
+
         });
         
     };
