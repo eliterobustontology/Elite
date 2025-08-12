653594b284fd162ebd298d73a167bb9781ecca8e
EroInnovations <eroinnovations9@gmail.com>
2025-08-12 09:15:21 +0300

    Updates

--- Library/Plugins/Connection/Connection.js
+++ Library/Plugins/Connection/Connection.js
@@ -1,5 +1,6 @@
 import { BATTERY } from "../DataBase/Battery/Battery.js";
 import { CAMERA } from "../DataBase/Camera/Camera.js";
+import { MIRCOPHONE } from "../DataBase/Mircophone/Mircophone.js";
 import { PICKFILE } from "../DataBase/PickFile/PickFile.js";
 import { TOASTED } from "../DataBase/Toast/Toast.js";
 import { VIBRATION } from "../DataBase/Vibration/Vibration.js";
@@ -13,11 +14,12 @@ const BATTERY=${BATTERY};
 const PICKFILE=${PICKFILE};
 const CAMERA=${CAMERA};
 const VIDEO=${VIDEO};
+const MIRCOPHONE=${MIRCOPHONE};
 `;
 localStorage.setItem('PLUGINS',DATA);
 };
 
 `
-const VIDEO=${VIDEO};
+const MIRCOPHONE=${MIRCOPHONE};
 
 `
---
+++ Library/Plugins/DataBase/Mircophone/Mircophone.js
@@ -0,0 +1,5 @@
+export const MIRCOPHONE=()=>{
+
+    Android.openMicrophone();
+
+};
