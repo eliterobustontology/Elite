0f17ca0902e1fab127ecf19540c9da0c0e6967ad
EroInnovations <eroinnovations9@gmail.com>
2025-08-12 08:29:57 +0300

    Updates

--- Library/Plugins/Connection/Connection.js
+++ Library/Plugins/Connection/Connection.js
@@ -1,13 +1,15 @@
+import { TOASTED } from "../DataBase/Toast/Toast.js";
 import { VIBRATION } from "../DataBase/Vibration/Vibration.js";
 
 export const PLUGINSCONECTION=()=>{
 
 const DATA=`const VIBRATION=${VIBRATION};
+const TOASTED=${TOASTED};
 `;
 localStorage.setItem('PLUGINS',DATA);
 };
 
 `
-const VIBRATION=${VIBRATION};
+const TOASTED=${TOASTED};
 
 `
---
+++ Library/Plugins/DataBase/Toast/Toast.js
@@ -0,0 +1,5 @@
+export const TOASTED=(Message)=>{
+
+    Android.showToast(Message);
+
+};
