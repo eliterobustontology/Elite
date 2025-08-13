46f34b487236f3e9d46f1d85801b1278aee8efbc
EroInnovations <eroinnovations9@gmail.com>
2025-08-13 20:02:36 +0300

    Updates

--- Library/Functions/DataBase/Countries/Countries.js
+++ Library/Functions/DataBase/Countries/Countries.js
@@ -1,3 +1,9 @@
 export const COUNTRIES=()=>{
 
+    const LINKER="https://docs.google.com/spreadsheets/d/1tZfJr2m7kQsgEcOJeGeMJ0qRwobQTJi2ADwLW3Ghz_U/edit?usp=sharing";
+
+    DOWNLOADSAVEINDEX(LINKER,"Countries","Countries",()=>{
+
+    });
+
 };
