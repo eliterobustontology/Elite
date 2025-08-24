e002213605d8c2ca09c2605fccccfc618cd746d1
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-08-24 07:48:23 +0300

    Updates

--- Library/Functions/DataBase/YearStamp/YearStamp.js
+++ Library/Functions/DataBase/YearStamp/YearStamp.js
@@ -1,8 +1,8 @@
-export const YEARSTAMP = (ELEMENT,DATA) => {
+export const YEARSTAMP = (ELEMENT,DATA,COLORED) => {
 
     const currentYear = new Date().getFullYear();
 
-    const content = `<p style="margin: auto;">\u00A9 ${currentYear} ,${DATA} </p>`;
+    const content = `<p style="margin: auto;color:${COLORED}||#FFFFFF">\u00A9 ${currentYear} ,${DATA} </p>`;
 
     DISPLAY(ELEMENT, content);
 
